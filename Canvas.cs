db88fffec1d49ee46fc668404e9272635558860a
Justynkem <justyna.motyka2004@wp.pl>
2024-06-13 19:00:40 +0200

    first commit

---
+++ Canvas.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SnakeTheGame
+{
+    public class Canvas
+    {
+        public int Width {get; set; }
+        public int Height {get; set;}
+        public Canvas()
+        {
+            Width = 100;
+            Height = 50;
+        }
+
+        public void drawCanvas()
+        {
+            Console.Clear();
+
+            for(int i = 0; i <= Width; i ++)
+            {
+                Console.SetCursorPosition(i, 0);
+                Console.Write("-");
+            }
+            for(int i = 0; i <= Width; i++)
+            {
+                Console.SetCursorPosition(i, Height);
+                Console.Write("-");
+            }
+            for(int i = 0; i <= Height; i++)
+            {
+                Console.SetCursorPosition(0, i);
+                Console.Write("-");
+            }
+            for(int i = 0; i <= Height; i++)
+            {
+                Console.SetCursorPosition(Width, i);
+                Console.Write("-");
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SnakeTheGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool finished = false;
+            Canvas canvas = new Canvas();
+
+            while(!finished)
+            {
+                canvas.drawCanvas();
+                Console.Read();
+            }
+        }
+    }
+}
